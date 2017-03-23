be47f4cbdf6f1ed870974afa68594914c0ca474b
mngweb <mangoweb@wp.pl>
2017-03-23 16:03:30 +0100

    Zadanie 3.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,67 @@
+/*
+5. Konwerter Markdown do HTML z File API
+Korzystając z File API stwórz konwerter formatu Markdown do formatu HTML. Twoim zadaniem jest stworzenie pola <input> o typie file, dzięki któremu użytkownik będzie mógł wybrać plik z formatem Markdown (zapisz treść dostępną tutaj do pliku lub stwórz własną), który automatycznie zostanie skonwertowany do kodu HTML (wykorzystaj do tego celu Showdown.js). Kod HTML, który otrzymasz po użyciu odpowiedniej metody z Showdown.js, wstaw w pole podglądu, które utworzysz. Możesz również dodać możliwość skopiowania wygenerowanego kodu HTML. Najłatwiej zrobić to wyświetlając pole <textarea>, do którego wstawisz owy kod, a następnie wywołasz na nim metodę .select()
+
+https://github.com/showdownjs/showdown
+*/
+
+
+
 
 (function() {
 
-    var positionOutput = document.querySelector("#position"),
-        linkOutput = document.querySelector("#link"),
-        button = document.querySelector("#button");
+    //PYTANIE 1: Jak zrobić aby oba divy zaczynały się w jednej lini?
 
+    //PYTANIE 2: Co można zrobić z wychodzącymi z diva fragmentami wynikowego htmla?
 
+   if(!window.FileReader) return; 
 
-    if (!navigator.geolocation) { 
-        positionOutput.innerHTML = "Ta przeglądarka nie wspiera Geolokalizacji!";
-    } 
+    var fileInput = document.querySelector("#fileInput"),
+        start = document.querySelector("#start"),
+        stop = document.querySelector("#stop"),
+        progress = document.querySelector("#progress"),
+        markupOutput = document.querySelector("#markupOutput"),
+        htmlOutput = document.querySelector("#htmlOutput");
 
+    fileInput.onchange = function() { 
 
-    function geoSuccess(position) {
+        var file = this.files[0],
+            reader = new FileReader();
 
-        /*   PYTANIE: czy dopisanie sp=point.latitude_longitude_titleString
-        (wg strony wg https://msdn.microsoft.com/en-us/library/dn217138.aspx )
-        jak w poniższej zakomentowanej linii nie powinno pokazywać jakiegoś markera - zamiast tego powoduje pojawienie się komunikatu o Udostępnionych miejscach z tekstem "Wystąpił problem z zapisaniem zmian w kolekcji. Spróbuj ponownie."    */
-
-        // var url = "http://bing.com/maps/default.aspx?cp=" + position.coords.latitude + "~" + position.coords.longitude + "&lvl=17&style=h&sp=point." + position.coords.latitude + "-" + position.coords.longitude + "_Jestes%20tutaj";
-
-
-        var url = "http://bing.com/maps/default.aspx?cp=" + position.coords.latitude + "~" + position.coords.longitude + "&lvl=17&style=h&rtp=pos." + position.coords.latitude + "_" + position.coords.longitude + "_Jestes%20tutaj";
-
-
-        positionOutput.innerHTML = "Twoja pozycja to: " + position.coords.latitude + ", " + position.coords.longitude;
-        linkOutput.innerHTML = "<a href='" + url + "' target='_blank'>Link do mapy Bing</a>";
-
-    }
-
-
-
-    function geoError(errorObj) {
-
-        var errorMessage;
-
-        switch (errorObj.code) {
-            case errorObj.PERMISSION_DENIED:
-                errorMessage = "Brak pozwolenia na znalezienie lokalizacji.";
-                break;
-
-            case errorObj.POSITION_UNAVAILABLE:
-                errorMessage = "Brak dostępu do sieci.";
-                break;
-
-            case errorObj.TIMEOUT:
-                errorMessage = "Przekroczono czas oczekiwania.";
-                break;
+        if(!file.type.match('text/plain')){
+        //if(!fileLink.name.match(".*\.txt")){
+             alert("Proszę wybrać plik tekstowy!");
         }
+        
 
-        positionOutput.innerHTML = "<strong>Wystąpił błąd: </strong>" + errorMessage;
+            reader.onprogress = function(e) {
+                if(e.lengthComputable) { 
+                    var percent = (e.loaded / e.total) * 100; 
 
-    }
+                    progress.value = percent; 
+                }
+            }
 
-    var options = { 
-        timeout: 1000
-    }
+            reader.onload = function(e) {
+                var converter = new showdown.Converter(),
+                    markup = this.result,
+                    html = converter.makeHtml(markup);
 
-   button.onclick = function() {
+                markupOutput.innerText = this.result;
+                //console.log(this.result);
 
-        positionOutput.innerHTML = "Czekaj...";
+                htmlOutput.innerText = html;
+                // console.log(html);
+            }
 
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError, options);
+
+            start.onclick = function() {
+                reader.readAsText(file);
+            }
+
+
+
+
     }
 
 })();
